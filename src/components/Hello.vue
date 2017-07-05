c7a15135b12d215adee50c06e6124c938a1eac9d
normandparatte <normand.paratte@gmail.com>
2017-07-05 19:48:15 +0200

    Récupération depuis l'API : - Récupération des images, synopsis et paragraphes grâce à une api (dans Hello.vue pour le moment) Divers : - Ajout de la règle pour les ";" dans la configuration de eslint - Installation de axios en --save donc ajout dans le fichier package.json

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,38 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div v-for="(titre , index) in titres" v-bind:key="index">        
+        <h1>
+          {{titre}}          
+        </h1>
+        <img v-bind:src="images[index]">
+        <p>{{synopsis[index]}}</p>   
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-  name: 'hello',
   data () {
+    let lien = 'http://image.tmdb.org/t/p/w185/';
+    axios.get(`https://amc.ig.he-arc.ch/tmdb/tv/popular?language=fr-CH`)
+    .then((response) => {
+      this.images = response.data.results.map(item => lien + item.poster_path);
+      this.titres = response.data.results.map(item => item.original_name);
+      this.synopsis = response.data.results.map(item => item.overview);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      images: [],
+      titres: []
+    };
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style>
+  
 </style>
