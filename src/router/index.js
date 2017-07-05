d1ee00311d7ae7a5bf0e6bb6bd834ef3c87f7dde
mvoyame <mael.voyame@he-arc.ch>
2017-07-05 17:31:00 +0200

    Ajout d'un exemple de VueMaterial

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import VueMaterial from 'vue-material'
 
 Vue.use(Router)
+Vue.use(VueMaterial)
 
 export default new Router({
   routes: [
