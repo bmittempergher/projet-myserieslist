fea684ca9e93e7471f57ff5982c46d536f440cbc
mvoyame <mael.voyame@he-arc.ch>
2017-07-05 20:40:21 +0200

    Correction de l'affichage des flexbox 	- Intégration dans une grid Diverses amélirations css

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,7 @@
   <div id="app">
     <router-view></router-view>
     <h2>Prochaine sortie</h2>
-    <md-layout md-gutter>
       <serie></serie>
-    </md-layout>
   </div>
 </template>
 
--- src/components/Serie.vue
+++ src/components/Serie.vue
@@ -1,17 +1,19 @@
 <template>
     <div>
-        <md-layout v-for="(titre , index) in titres" v-bind:key="index" md-flex-xsmall="100" md-flex-small="50" md-flex-medium="33" md-flex-large="33">
-            <md-card>
-                <md-card-header>
-                    <div class="md-title">{{titre}}</div>
-                </md-card-header>
-                <md-card-media>
-                    <img v-bind:src="images[index]" style="width: 200px; height: 200px;">
-                </md-card-media>
-                <md-card-content>
-                    {{synopsis[index]}}
-                </md-card-content>
-            </md-card>
+        <md-layout md-gutter>
+            <md-layout v-for="(titre , index) in titres" v-bind:key="index" md-flex-xsmall="100" md-flex-small="50" md-flex-medium="33" md-flex-large="33">
+                <md-card style="max-width: 320px; display: inline-block; padding: 16px; margin: 16px;">
+                    <md-card-header>
+                        <div class="md-title">{{titre}}</div>
+                    </md-card-header>
+                    <md-card-media>
+                        <img v-bind:src="images[index]" style="width: 200px; height: 200px; margin: auto; display: block;">
+                    </md-card-media>
+                    <md-card-content>
+                        {{synopsis[index]}}
+                    </md-card-content>
+                </md-card>
+            </md-layout>
         </md-layout>
     </div>
 </template>
