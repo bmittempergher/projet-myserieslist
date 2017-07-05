b22a240858bdde1d25462009c483749bd5652f33
mvoyame <mael.voyame@he-arc.ch>
2017-07-05 20:13:17 +0200

    Intégration de l'API 	- Dans Serie.vue
    Ajout de la boucle v-for pour lister les séries
    	- Dans Serie.vue

--- src/components/Serie.vue
+++ src/components/Serie.vue
@@ -1,21 +1,41 @@
 <template>
-    <md-layout md-flex-xsmall="100" md-flex-small="50" md-flex-medium="33" md-flex-large="33">
-        <md-card>
-            <md-card-header>
-                <div class="md-title">Breaking Bad</div>
-            </md-card-header>
-            <md-card-media>
-                <img v-bind:src="serie | toImage" style="width: 200px; height: 200px;">
-            </md-card-media>
-            <md-card-content>
-                LOREM IPSUM
-            </md-card-content>
-        </md-card>
-    </md-layout>
+    <div>
+        <md-layout v-for="(titre , index) in titres" v-bind:key="index" md-flex-xsmall="100" md-flex-small="50" md-flex-medium="33" md-flex-large="33">
+            <md-card>
+                <md-card-header>
+                    <div class="md-title">{{titre}}</div>
+                </md-card-header>
+                <md-card-media>
+                    <img v-bind:src="images[index]" style="width: 200px; height: 200px;">
+                </md-card-media>
+                <md-card-content>
+                    {{synopsis[index]}}
+                </md-card-content>
+            </md-card>
+        </md-layout>
+    </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+  data () {
+    let lien = 'http://image.tmdb.org/t/p/w185/';
+    axios.get(`https://amc.ig.he-arc.ch/tmdb/tv/popular?language=fr-CH`)
+    .then((response) => {
+      this.images = response.data.results.map(item => lien + item.poster_path);
+      this.titres = response.data.results.map(item => item.original_name);
+      this.synopsis = response.data.results.map(item => item.overview);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+    return {
+      images: [],
+      titres: []
+    };
+  },
   props: ['serie', 'ind'],
   filters: {
     toImage (serie) {
