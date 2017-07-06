209977e8e32b1b00fc3761a4c3c350590da10d44
normandparatte <normand.paratte@gmail.com>
2017-07-06 11:49:47 +0200

    Affichage des détail d'une série

--- src/components/SerieDetail.vue
+++ src/components/SerieDetail.vue
@@ -1,9 +1,28 @@
 <template>
     <div>
-        <h1>{{serie.titre}}</h1>
-        <img :src="serie.image">
-        <h2>Note : {{serie.note}}</h2>
-        <p>{{serie.synopsis}}</p>
+      <div class="cover">
+        <md-layout md-gutter>
+          <md-layout md-flex-xsmall="100" md-flex-small="100" md-flex-medium="100">
+            <h1>{{serie.titre}}</h1>
+          </md-layout>
+        </md-layout>
+      </div>
+      <div class="infos">
+        <md-layout row>
+          <p>{{serie.synopsis}}</p>
+        </md-layout>
+        <md-layout row>          
+          <md-layout md-flex-xsmall="100" md-flex-small="50" md-flex-medium="33">
+              <h2>Note : {{serie.note}}</h2>
+          </md-layout>
+          <md-layout md-flex-xsmall="100" md-flex-small="50" md-flex-medium="33">
+            <h2>Genre</h2>
+          </md-layout>
+          <md-layout md-flex-xsmall="100" md-flex-small="50" md-flex-medium="33">
+            <h2>Date de sortie</h2>
+          </md-layout>
+        </md-layout>       
+      </div>
     </div>
 </template>
 
@@ -15,12 +34,13 @@ export default {
       .then((response) => {
         let serie = {};
         serie.id = response.data.id;
-        serie.image = 'http://image.tmdb.org/t/p/w185' + response.data.poster_path;
+        serie.image = 'http://image.tmdb.org/t/p/original' + response.data.backdrop_path;
         serie.synopsis = response.data.overview;
         serie.note = response.data.vote_average / 2;
         serie.titre = response.data.name;
 
         this.serie = serie;
+        document.documentElement.style.setProperty('--img', 'url("' + serie.image + '"');
       })
       .catch(error => {
         console.log(error);
@@ -34,5 +54,27 @@ export default {
 </script>
 
 <style>
-  
+:root{
+  --img: '';
+}
+
+.cover{
+  background-image: var(--img);
+  min-height: 600px;
+  background-size: cover;
+  color: white;
+  background-color: black;
+}
+.cover h1{
+  margin-top:0;
+  padding-top:50px;
+}
+
+.cover p{
+  margin-top:400px;
+}
+
+.content-grid {
+  max-width: 960px;
+}
 </style>
